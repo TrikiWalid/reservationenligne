10fe2117754d3c68f8dfee9212eb81e4096e203d
sti2 <sti2@IGA-DEV45.iga.tn.com>
2016-11-17 17:08:22 +0100

    17/11/2016

---
+++ reservationenligne/src/main/java/com/informatec/reservationenligne/controller/AppConfig.java
@@ -0,0 +1,12 @@
+package com.informatec.reservationenligne.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.informatec.reservationenligne")
+public class AppConfig {
+
+}
